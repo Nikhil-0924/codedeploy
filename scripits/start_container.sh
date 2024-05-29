3e6593079c40c09fe98558d3c2b6a57368eb7e2b
Bharothu Nikhil Naik <110222551+nikhil45678@users.noreply.github.com>
2024-05-29 19:48:19 +0530

    Delete scripits directory

--- scripits/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -dt -p 5000:5000 abhishekf5/simple-python-flask-app
